120b3826b7dd6d00050909421242851b6f08e26e
faomg201 <69358628+faomg201@users.noreply.github.com>
2022-05-27 17:31:59 +0700

    ch scrollbar + add blog**comment

--- src/components/AllPage.js
+++ src/components/AllPage.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import { NavBar } from "./NavBar";
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import Serve from "./Serve";
 import Works from "./Works";
@@ -7,21 +8,38 @@ import Abouts from "./Abouts";
 import Contact from "./Contact";
 
 export default function AllPage() {
+    const ScrollNav = () => {
+        const myRef1 = useRef(null);
+        const myRef2 = useRef(null);
+
+        const executeScroll = (id) => {
+            switch (id) {
+                case '1':
+                    myRef1?.current.scrollIntoView({ behavior: 'smooth' });
+                    break;
+                case '2':
+                    myRef2?.current.scrollIntoView({ behavior: 'smooth' });
+                    break;
+            }
+        }
+    }
     return (
         <>
-            <NavBar />
-            <div id="ServeScroll" style={{overflow:'hidden'}}>
-                <Serve />
-            </div>
-            <div id="WorksScroll" style={{overflow:'hidden'}}>
-                <Works />
-            </div>
-            <div id="AboutsScroll" style={{overflow:'hidden'}}>
-                <Abouts />
-            </div>
-            <div id="ContactScroll" style={{overflow:'hidden'}}>
-                <Contact />
-            </div>
+            <Scrollbars autoHide='true' style={{ width: '100vw', height: '100vh' }}>
+                <NavBar />
+                <div id="ServeScroll" style={{ overflow: 'hidden' }}>
+                    <Serve />
+                </div>
+                <div id="WorksScroll" style={{ overflow: 'hidden' }}>
+                    <Works />
+                </div>
+                <div id="AboutsScroll" style={{ overflow: 'hidden' }}>
+                    <Abouts />
+                </div>
+                <div id="ContactScroll" style={{ overflow: 'hidden' }}>
+                    <Contact />
+                </div>
+            </Scrollbars>
         </>
     );
 }
